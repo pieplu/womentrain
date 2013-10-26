9258be04e7e03439824889af6f4c6f3a54440560
Pieplu <alexis.pieplu@gmail.com>
2013-10-26 03:17:32 -0400

    optimisé

--- src/Solution.java
+++ src/Solution.java
@@ -8,69 +8,59 @@ import javax.swing.plaf.basic.BasicInternalFrameTitlePane.RestoreAction;
 
 public class Solution {
 	
-	
-	static double timeAll;
 	static double timeDec;
     
-	public static void acceleration()
-	{
-		
-		float dt = 0.01f;     		// frequence d'echantillonage en secondes
+	public static double distanceAcceleration(){
+		float dt = 0.0005f;     	// frequence d'echantillonage en secondes
 		float accel = 0.75f; 		// m/s²
 		double speed = 0.0;         // m/s
 		double position = 0.0;      // m
 		double time;
-		
+		double distTemp;
 		double rest_position = 100000;
-	 
-			for (time = 0.0; deceleration(speed, rest_position) >= 0; time += dt)   
-				{
-					
-					position += (accel * dt * dt / 2.0) + speed * dt;
-					rest_position -= (accel * dt * dt / 2.0) + speed * dt;
-					if (speed < 25){
-						speed += accel * dt;
-				}
-					//System.out.println("-----\t" + time + " | " + speed + " | " + position);
-					
-					
-				}
-			
-			timeAll = time + timeDec;
 		
+			for (time = 0.0; distanceDecceleration(speed, rest_position) >= 0; time += dt){
+				distTemp = (accel * dt * dt / 2.00) + speed * dt;
+				position += distTemp;
+				rest_position -= distTemp;
+				if (speed < 25){
+					speed += accel * dt;
+				}
+				//System.out.println("-----\t" + time + " | " + speed + " | " + position);
+			}
+		return time + timeDec;
 	}
 	
 	
-	public static double deceleration(double speed, double rest_position)
-	{
-		
-		float dt = 0.01f;     // frequence d'echantillonage en secondes
-		float decel = 1.05555555555555556f; // m/s²
-
+	public static double distanceDecceleration(double speed, double rest_position){
+		float dt = 0.5f;     	// frequence d'echantillonage en secondes
+		float decel = 1.05556f; // m/s²
+		double restPourDt = rest_position;
 		double temps;
 		
-			for (temps = 0.0; speed >= 0; temps += dt)   // Termine au bout de 2.5 secondes
-				{
-					//System.out.println( "*****\t" + time + "-" + speed + "-" + rest_position);
-					rest_position -= (decel * dt * dt / 2.0) + speed * dt;
-					speed -= decel * dt;
-				}		
-			timeDec = temps;
-		return rest_position;
-		
+		for (temps = 0.0; speed >= 0; temps += dt){
+			if(restPourDt<1000){dt = 0.005f;
+			}else if(restPourDt<500){dt= 0.0005f;
+			}else if(restPourDt<200){dt = 0.000005f;
+			}
+			
+			//System.out.println( "*****\t" + time + "-" + speed + "-" + rest_position);
+			rest_position -= (decel * dt * dt / 2.00) + speed * dt;
+			speed -= decel * dt;
+		}		
+		timeDec = temps;
+		return rest_position;	
 	}
 	
 	public static void main(String[] args) {
 		
 		int temps_depard = 0;
 		int temps_arrive = 0;
-		
 		int nbr_train = 0;
 		int nbr_sections = 0;
 		
-		acceleration();
 
-		System.out.println(timeAll);
+		System.out.println(distanceAcceleration());
 		
 		output(temps_depard, temps_arrive, nbr_train, nbr_sections);
 		System.out.println("x");
@@ -81,12 +71,6 @@ public class Solution {
     }
 
 
-	/**
-	 * @param temps_depard
-	 * @param temps_arrive
-	 * @param nbr_train
-	 * @param nbr_sections
-	 */
 	private static void output(int temps_depard, int temps_arrive, int nbr_train, int nbr_sections) {
 		for (int i = 0;i < nbr_sections; i++)
 		{
